77601116fc38fd0b33551278129e0e4f3bfb43f6
Vladimir <verygin@stream.ru>
2020-06-24 19:11:56 +0300

    moved multicast module to separate branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { CardsContainerComponent } from './cards-container/cards-container.component';
 
 const routes: Routes = [
-    {
-        path: 'layout',
-        loadChildren: () => import('../app/multicasting/multicasting.module').then((m) => m.MulticastingModule),
-    },
     { path: '', component: CardsContainerComponent, pathMatch: 'full' },
     { path: ':filter', component: CardsContainerComponent },
 ];
